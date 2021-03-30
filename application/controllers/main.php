067f2e6522f0f692852d4bc2cd9417ce59b22afe
Nop-Nop <nabyan98.na@Gmail.com>
2021-03-30 09:57:38 +0700

    test

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class control extends CI_Controller {
+class main extends CI_Controller {
 	public function index(){
 		$this->load->view('auth/login');
 	}
